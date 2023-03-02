9d3c2f8b87708669514e66b978bef3d83815f3b5
Zachary Rojas <ztr2194@gmail.com>
2023-03-02 09:08:34 -0600

    array lab complete

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat() {
+    cats.push('Ralph');
+}
+function destructivelyPrependCat() {
+    cats.unshift('Bob');
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) { 
+    const catsCopy = cats.slice();
+    catsCopy.push(name);
+    return catsCopy;
+}
+function prependCat(name) {
+    const catscopy = cats.slice();
+    catscopy.unshift(name);
+    return catscopy;
+}
+function removeLastCat() {
+    const catscopy = cats.slice();
+    catscopy.pop();
+    return catscopy;
+}
+function removeFirstCat() {
+    const catscopy = cats.slice();
+    catscopy.shift();
+    return catscopy;
+}
